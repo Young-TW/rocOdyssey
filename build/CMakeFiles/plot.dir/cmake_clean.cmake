32622af8ba3cbc648670c34d3ba6d08e7b646f98
Young <young20050727@gmail.com>
2025-11-23 22:39:43 +0800

    git: Update .gitignore

--- build/CMakeFiles/plot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/plot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/plot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
